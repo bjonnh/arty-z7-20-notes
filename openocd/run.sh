31497c3b8553e8687c8ac6636609bc2314e97f58
Jonathan Bisson <bjonnh-ruby@bjonnh.net>
2021-12-06 18:37:59 -0600

    Push bitstream 10 times faster, because we can

--- openocd/run.sh
+++ openocd/run.sh
@@ -1,1 +1,1 @@
-openocd -f digilent-arty-z7.cfg -f /usr/share/openocd/scripts/target/zynq_7000.cfg -c "init; pld load 0 blinky.bit; exit"
+openocd -f digilent-arty-z7.cfg -f zynq_7000.cfg -c "init; pld load 0 blinky.bit; exit"
